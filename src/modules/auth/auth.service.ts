b7c12dcb9ecd5f7ea7c09972b70c7ef36821c5a4
shadhin <shadhin.int@gmail.com>
2021-06-28 03:17:41 +0600

    implementing auth service

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from '../users/user.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly userService: UserService) {}
+
+  async validateUser(username: string, pass: string) {
+    const user = await this.userService.findOneByEmail(username);
+
+    if (!user) {
+      return null;
+    }
+
+    const match = await this.comparePassword(pass, user.password);
+    if (!match) {
+      return null;
+    }
+
+    // tslint:disable-next-line: no-string-literal
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...result } = user['dataValues'];
+    return result;
+  }
+
+  private async comparePassword(enteredPassword, dbPassword) {
+    const match = await bcrypt.compare(enteredPassword, dbPassword);
+    return match;
+  }
+}
